24850e103a32a03cf605f6bdcd646cbc3df42b74
XineBSD <136019664+XineBSD@users.noreply.github.com>
2023-06-21 18:49:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-mv sigma /etc
+#!/usr/bin/bash
+
+rm -rf /etc/sigma
+echo "$(grep -v 'sigma="exec /etc/sigma/main.rb"' ~/.bashrc)" > ~/.sigmatrash
+
+cp sigma /etc
 echo alias sigma="exec /etc/sigma/main.rb" >> ~/.bashrc
+echo Success!
